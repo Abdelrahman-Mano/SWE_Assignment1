a34d3c4cb447b8795bc6e88b65f925b1f010bf49
Abdelrahman Ayman <abdelrahman2652006@gmail.com>
2026-03-03 11:11:30 +0200

    Add files via upload

---
+++ MANO Finance Tracker/Expense.java
@@ -0,0 +1,12 @@
+public class Expense {
+
+    private double amount;
+
+    public Expense(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
